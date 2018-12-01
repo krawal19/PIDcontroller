a6c7d2491d60b74f79985046af2889b8451620af
krawal19 <kapilrawal1995@gmail.com>
2018-11-30 22:40:03 -0500

    Added testPID file containing gmock test functionalities with google test

--- test/testPID.cpp
+++ test/testPID.cpp
@@ -1,42 +1,93 @@
 /**
- *  @file      testPID.cpp
- *  @brief     Unit test for PID Class
- *  @details   Unit test implementation to test PID class
- *  @author    Saimouli Katragadda
- *  @copyright GNU Public License.
+ *
+ * BSD 3-Clause License
+ * Copyright (c) 2018, Kapil Rawal.
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  @copyright (c) BSD
+ *
+ *  @file    testPID.cpp
+ *
+ *  @author  Kapil Rawal
+ *
+ *  @copyright BSD License
+ *
+ *  @brief   Unit test
+ *
+ *  @section DESCRIPTION
+ *
+ *  This programs implements Unit test to test PID class by mocking 
+ *  Parameters class
  */
+
+
 #include <gtest/gtest.h>
+#include "gmock/gmock.h"
 #include <iostream>
 #include <memory>
 #include "PID.h"
+#include "Parameters.h"
+using ::testing::Return;
+using ::testing::AtLeast;
+/**
+ * @brief Mock of class Parameters
+ */
+class MockParameters : public Parameters {
+ public:
+  MOCK_METHOD0(getKP, double());
+  MOCK_METHOD0(getKD, double());
+  MOCK_METHOD0(getKI, double());
+};
 
 /**
- * @brief Tests pid compute method
+ * @brief Checking using GMOCK if object methods are called. 
  */
-TEST(PIDtest, computeMethodPidTest) {
-  std::shared_ptr<PID> pid = std::make_shared<PID>(2, 1, 0.1, 0.5);
-
-  float newVel = 0;
-  // setpoint velocity is 1m/s and current velocity is 2m/s
-  newVel = pid->computePID(1, 2);
-
-  // the compute method should output a value of -2.5m/s
-  EXPECT_NEAR(-4.05, newVel, 0.1);
+TEST(PIDTest,Test) {
+  MockParameters param;
+  /// Expect call to KP at least once
+  EXPECT_CALL(param, getKP()).Times(AtLeast(1));
+  /// Expect call to KD at least once
+  EXPECT_CALL(param, getKD()).Times(AtLeast(1));
+  /// Expect call to KI at least once
+  EXPECT_CALL(param, getKI()).Times(AtLeast(1));
+  PID pid(&param);
+  pid.computePID(1,2);
 }
 
 /**
- * @brief Tests pid gains initializing method
+ * @brief Checking if computePID is working by using GMOCK.
  */
-
-TEST(PIDtest, SetGainsTest) {
-  std::shared_ptr<PID> pid = std::make_shared<PID>();
-  // setting the gains using setKp method
-  pid->setKP(3);
-  pid->setKD(2);
-  pid->setKI(4);
-  // checking if the gains are properly set by Setkp method
-  EXPECT_EQ(3, pid->getKP());
-  EXPECT_EQ(2, pid->getKD());
-  // std::cout << "test kd: " << kd << std::endl;
-  EXPECT_EQ(4, pid->getKI());
+TEST(PIDTest, adjustvalue) {
+ MockParameters param;
+  /// Calling KP once 
+  EXPECT_CALL(param, getKP())  
+      .Times(1).WillRepeatedly(Return(2));
+  /// Calling KD once
+  EXPECT_CALL(param, getKI())  
+      .Times(1).WillRepeatedly(Return(1));
+  /// Calling KI once
+  EXPECT_CALL(param, getKD())  
+      .Times(1).WillRepeatedly(Return(0.1));
+  PID instPID(&param);
+EXPECT_NEAR(-3.5, instPID.computePID(1, 2),0.5);
 }
